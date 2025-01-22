5f2d2681d110fb7bdce8aa70258b743cfcd5ea03
Pharaoh Tornes <155219985+pharaohmadeit@users.noreply.github.com>
2025-01-21 20:18:40 -0800

    Update linear_search.py

--- linear_search.py
+++ linear_search.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+"""Sorting a numpy array object with linear sort algorithm."""
 def linear_search(data, search_key):
     for index, value in enumerate(data):
         if value == search_key:
